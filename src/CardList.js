5ff4c35cbffe10226e94da5351c23ed34a9488a1
India Hiring <indiahiring@global.publicisgroupe.net>
2019-12-21 16:41:20 +0530

    added sort by id style

--- src/CardList.js
+++ src/CardList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, withStyles, Select, MenuItem   } from '@material-ui/core';
+import { Grid, withStyles, Select, MenuItem, FormControl, InputLabel } from '@material-ui/core';
 
 import Card from './CardComponent';
 
@@ -10,6 +10,11 @@ const styles = (theme) => ({
     color: theme.palette.text.secondary,
     marginTop: 15,
   },
+  alien: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    display: 'flex',
+  }
 });
 
 class CardList extends React.Component {
@@ -19,14 +24,18 @@ class CardList extends React.Component {
       <div className={classes.paper}>
         <Grid container spacing={2} alignItems="stretch" direction="row">
             {data.length <= 0 ? 'No Data Found' : ''}
-            <Grid item xs={12} sm={10}>
-              <Select
-                value={this.props.sortBy}
-                onChange={(e) => this.props.sortData(e.target.value)}
-              >
-                <MenuItem value="asc">asc</MenuItem >
-                <MenuItem value="dsc">dsc</MenuItem >
-              </Select>
+            <Grid item xs={12} sm={12} className={classes.alien}>
+            <FormControl className={classes.formControl}>
+              <InputLabel id="sort">Sort by ID</InputLabel>
+                <Select
+                  labelId="sort"
+                  value={this.props.sortBy}
+                  onChange={(e) => this.props.sortData(e.target.value)}
+                >
+                  <MenuItem value="asc">Ascending</MenuItem >
+                  <MenuItem value="dsc">Descending</MenuItem >
+                </Select>
+              </FormControl>
             </Grid>
             {data.map(d => <Grid item xs={6} sm={3} key={d.id}><Card data={d} /></Grid>)}
         </Grid>
